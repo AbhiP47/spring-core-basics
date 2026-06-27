a3d34dc4d6389e65cad47d126430cbd30df09f36
AbhiP47 <abhinavjaipanwar@gmail.com>
2026-06-27 16:16:34 +0530

    jar dependency

---
+++ jarDependency/src/main/java/com/abhinav/jarDependency/CartService.java
@@ -0,0 +1,8 @@
+package com.abhinav.jarDependency;
+
+public class CartService {
+    public void addToCart()
+    {
+        System.out.println("Added To Cart");
+    }
+}
---
+++ jarDependency/src/main/java/com/abhinav/jarDependency/JarDependencyApplication.java
@@ -0,0 +1,9 @@
+package com.abhinav.jarDependency;
+
+
+public class JarDependencyApplication {
+
+	public static void main(String[] args) {
+	}
+
+}
