7748f70edb3efcfb5c6bc2c5bd96abf1b4ff1691
AbhiP47 <abhinavjaipanwar@gmail.com>
2026-06-27 22:59:59 +0530

    Bean Initialization

---
+++ BeanInitialization/src/main/java/org/abhinav/AppConfig.java
@@ -0,0 +1,9 @@
+package org.abhinav;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan
+@Configuration
+public class AppConfig {
+}
