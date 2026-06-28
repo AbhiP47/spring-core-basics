68cd4619a061e2fe7191df495a0fe85fad78b587
AbhiP47 <abhinavjaipanwar@gmail.com>
2026-06-28 13:33:22 +0530

    bean lifecycle

---
+++ BeanLifecycle/src/main/java/org/abhinav/AppConfig.java
@@ -0,0 +1,9 @@
+package org.abhinav;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.abhinav")
+public class AppConfig {
+}
