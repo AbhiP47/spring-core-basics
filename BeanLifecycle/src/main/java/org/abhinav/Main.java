8a70d24f7a861204db9e1f047baf37cb395f1751
AbhiP47 <abhinavjaipanwar@gmail.com>
2026-06-29 15:31:39 +0530

    spring config

--- BeanLifecycle/src/main/java/org/abhinav/Main.java
+++ BeanLifecycle/src/main/java/org/abhinav/Main.java
@@ -1,6 +1,5 @@
 package org.abhinav;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
