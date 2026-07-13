de66f5aa08efa652ba12fa0d59da5a6cfb662720
AbhiP47 <abhinavjaipanwar@gmail.com>
2026-07-13 21:00:14 +0530

    service & repo

--- exceptionHandling/src/main/java/com/abhinav/exceptionHandling/repository/StudentRepo.java
+++
@@ -1,4 +0,0 @@
-package com.abhinav.exceptionHandling.repository;
-
-public class StudentRepo {
-}
---
+++ exceptionHandling/src/main/java/com/abhinav/exceptionHandling/repository/StudentRepository.java
@@ -0,0 +1,20 @@
+package com.abhinav.exceptionHandling.repository;
+
+
+import com.abhinav.exceptionHandling.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+//@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    Optional<Student> findByIdAndDeletedIsFalse(Long id);
+
+    List<Student> findByDeletedIsFalse();
+
+    Boolean existsByEmail(String emailId);
+
+    // findBy + fieldName + condition
+}
