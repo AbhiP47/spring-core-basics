50add32bb92732ad5929bbb4a5f773d82010af1c
AbhiP47 <abhinavjaipanwar@gmail.com>
2026-07-15 21:40:07 +0530

    filter notes

--- filters/src/main/java/com/abhinav/filters/entity/Student.java
+++ filters/src/main/java/com/abhinav/filters/entity/Student.java
@@ -1,4 +1,29 @@
 package com.abhinav.filters.entity;
 
 public class Student {
+    private int email;
+    private int name;
+
+    public int getName() {
+        return name;
+    }
+
+    public Student setName(int name) {
+        this.name = name;
+        return this;
+    }
+
+    public int getEmail() {
+        return email;
+    }
+
+    public Student setEmail(int email) {
+        this.email = email;
+        return this;
+    }
+
+    public Student(int name, int email) {
+        this.name = name;
+        this.email = email;
+    }
 }
--- filters/src/main/java/com/abhinav/filters/service/StudentService.java
+++ filters/src/main/java/com/abhinav/filters/service/StudentService.java
@@ -1,4 +1,19 @@
 package com.abhinav.filters.service;
 
+import com.abhinav.filters.entity.Student;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentService {
+
+    public void createStudent(Student student) {
+        System.out.println("Student Created");
+        System.out.println(student.getName());
+        System.out.println(student.getEmail());
+
+//        try {
+//            Thread.sleep(2000);
+//        }
+//        catch(Exception e) {}
+    }
 }
