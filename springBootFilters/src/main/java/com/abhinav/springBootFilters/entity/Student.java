c91904bddf5e28701585d0922ed93a503204c81d
AbhiP47 <abhinavjaipanwar@gmail.com>
2026-07-19 18:12:29 +0530

    classes

---
+++ springBootFilters/src/main/java/com/abhinav/springBootFilters/entity/Student.java
@@ -0,0 +1,34 @@
+package com.abhinav.springBootFilters.entity;
+
+public class Student {
+    private int id;
+    private String email;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public Student setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Student setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Student setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
