b84ab66944a97bf6dff70b98fda8e26cc32b3f51
AbhiP47 <abhinavjaipanwar@gmail.com>
2026-07-19 19:08:24 +0530

    wrapper notes

--- springBootFilters/src/main/java/com/abhinav/springBootFilters/filters/RequestFilter.java
+++ springBootFilters/src/main/java/com/abhinav/springBootFilters/filters/RequestFilter.java
@@ -1,4 +1,18 @@
 package com.abhinav.springBootFilters.filters;
 
-public class RequestFilter {
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class RequestFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        
+    }
 }
