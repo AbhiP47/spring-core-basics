cccf76e529f03f95688419b300ead88e39751d44
AbhiP47 <abhinavjaipanwar@gmail.com>
2026-07-23 02:51:16 +0530

    Logging Handler

---
+++ interceptors/src/main/java/com/abhinav/interceptors/interceptor/LoggingInterceptor.java
@@ -0,0 +1,36 @@
+package com.abhinav.interceptors.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    @Override
+   public boolean preHandle(HttpServletRequest request,
+                            HttpServletResponse response,
+                            Object handler) throws Exception {
+        System.out.println("Pre Handled Called");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response,
+                           Object handler,
+                           @Nullable ModelAndView modelAndView) throws Exception {
+        System.out.println("Post handle called");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response,
+                                Object handler,
+                                @Nullable Exception ex) throws Exception {
+        System.out.println("After Completion called");
+    }
+}
---
+++ interceptors/src/main/java/com/abhinav/interceptors/service/StudentService.java
@@ -0,0 +1,21 @@
+package com.abhinav.interceptors.service;
+
+
+import com.abhinav.interceptors.entity.Student;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    public void createStudent(Student student) {
+        System.out.println("Student Created");
+        System.out.println(student.getName());
+        System.out.println(student.getEmail());
+
+        try {
+           Thread.sleep(2000);
+       }
+       catch(Exception e) {}
+
+    }
+}
