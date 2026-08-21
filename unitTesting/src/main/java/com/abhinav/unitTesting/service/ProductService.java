3a5344b151d9af3c1da8e43db04449a8a351a178
AbhiP47 <abhinavjaipanwar@gmail.com>
2026-08-21 20:09:40 +0530

    unit testing

---
+++ unitTesting/src/main/java/com/abhinav/unitTesting/service/ProductService.java
@@ -0,0 +1,37 @@
+package com.abhinav.unitTesting.service;
+
+import com.abhinav.unitTesting.entity.Product;
+import com.abhinav.unitTesting.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product getProductById(Long id) {
+
+        return productRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Product not found: " + id)
+                );
+    }
+
+    public Product createProduct(Product product) {
+
+        boolean alreadyExists =
+                productRepository.existsByName(product.getName());
+
+        if (alreadyExists) {
+            throw new IllegalArgumentException(
+                    "Product already exists"
+            );
+        }
+
+        return productRepository.save(product);
+    }
+}
