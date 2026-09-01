a88eabf77ade6a0388ec18ab4a3c96f1c0583812
AbhiP47 <abhinavjaipanwar@gmail.com>
2026-09-01 16:19:44 +0530

    2.transactions service methods

--- transactions/src/main/java/com/abhinav/transactions/service/OrderService.java
+++ transactions/src/main/java/com/abhinav/transactions/service/OrderService.java
@@ -2,6 +2,7 @@ package com.abhinav.transactions.service;
 
 import com.abhinav.transactions.entity.Order;
 import com.abhinav.transactions.repository.OrderRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +11,14 @@ public class OrderService {
 
     @Autowired
     private  OrderRepository orderRepository;
+    @Autowired
+    private  PaymentAuditService paymentAuditService;
 
+
+    @Transactional
     public void placeOrder(Order order)
     {
-
+            orderRepository.save(order);
+            paymentAuditService.audit(order);
     }
 }
--- transactions/src/main/java/com/abhinav/transactions/service/PaymentAuditService.java
+++ transactions/src/main/java/com/abhinav/transactions/service/PaymentAuditService.java
@@ -1,7 +1,23 @@
 package com.abhinav.transactions.service;
 
+import com.abhinav.transactions.entity.Order;
+import com.abhinav.transactions.entity.PaymentAudit;
+import com.abhinav.transactions.repository.PaymentAuditRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentAuditService {
+
+    private final PaymentAuditRepository paymentAuditRepository;
+
+    public PaymentAuditService(PaymentAuditRepository paymentAuditRepository) {
+        this.paymentAuditRepository = paymentAuditRepository;
+    }
+
+    public void audit(Order order) {
+
+        PaymentAudit paymentAudit =
+                new PaymentAudit(order.getAmount() , order.getId(), true);
+        paymentAuditRepository.save(paymentAudit);
+    }
 }
